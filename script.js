8c83ba12b1279daba661ef8606963d6e36d3cbf7
Nanami7770 <hatananax0707@icloud.com>
2025-11-24 21:47:37 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("送信ありがとうございました！");
+});
+
+document.getElementById("applyForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("応募を受け付けました！");
+});
